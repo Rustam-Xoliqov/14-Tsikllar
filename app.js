890c7ff7783f7e4dc1185c9fd8812c5da94e4aca
Rustam <rustamxoliqov777@gmail.com>
2023-02-28 05:27:15 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let startNum = 1;
+
+// First->
+// while (startNum <= 100) {
+//     console.log(startNum);
+//     startNum++;
+// };
+
+// Second
+// do {
+//     console.log(startNum);
+//     startNum++;
+// } while (startNum <= 10);
+
+// Third
+for (let i = 0; i <= 10; i++) {
+    if (i === 8) {
+        //break;
+        continue;
+    }
+    console.log(i)
+    
+};
